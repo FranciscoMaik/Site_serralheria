b4cea407c761ec80644bfa5734dd2e222c45942b
FranciscoMaik <chiconunes3081@gmail.com>
2019-10-27 17:48:19 -0300

    botão no login

---
+++ minigram/public/javascripts/fire.js
@@ -0,0 +1,16 @@
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCmZZz3Nx4NAzzyOsDuWb_UwLrsBjnN4h4",
+    authDomain: "site-empresa-8ba65.firebaseapp.com",
+    databaseURL: "https://site-empresa-8ba65.firebaseio.com",
+    projectId: "site-empresa-8ba65",
+    storageBucket: "site-empresa-8ba65.appspot.com",
+    messagingSenderId: "170153145004",
+    appId: "1:170153145004:web:a91586749bcc5414092509",
+    measurementId: "G-GSY251S49F"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+</script>
